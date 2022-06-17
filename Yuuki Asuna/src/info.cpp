2bec8b8b8ff9095e1a809ee63d90d5071c280deb
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-06-17 23:40:47 +0700

    it runs ayy

--- Yuuki Asuna/src/info.cpp
+++ Yuuki Asuna/src/info.cpp
@@ -10,45 +10,48 @@ void info_h(dpp::cluster& client, const dpp::slashcommand_t& event)
 	/* ------------------- I still making it, please wait ------------------- */
 
 	// The info command will be used menu system, we have a lot of info!
-	client.on_message_create([&client](const dpp::message_create_t& event) {
-		
-		dpp::message content("Please choose a category you'd like to know");
+	dpp::message content("Please choose a category you'd like to know");
 
-		// Add component
-		content.add_component(
-			dpp::component().add_component(
-				dpp::component().set_type(dpp::cot_selectmenu)
-					.set_placeholder("Information categories")
-					.add_select_option(
-						dpp::select_option(
-							"About me", 
-							"about", 
-							"About this bot"
-						))
-					.add_select_option(
-						dpp::select_option(
-							"Avatar and Username", 
-							"avatar_username",
-							"Information about this bot avatar and username"
-						))
-					.set_id("information")
-			)
-		);
+	// Add component
+	content.add_component(
+		dpp::component().add_component(
+			dpp::component().set_type(dpp::cot_selectmenu)
+			.set_placeholder("Information categories")
+			.add_select_option(
+				dpp::select_option(
+					"About me",
+					"about",
+					"About this bot"
+				))
+			.add_select_option(
+				dpp::select_option(
+					"Avatar and Username",
+					"avatar_username",
+					"Information about this bot avatar and username"
+				))
+			.set_id("information")
+		)
+	);
 
-		// Create a message with the content
-		event.reply(
-			content.set_flags(dpp::m_ephemeral)
-		);
-
-		});
-
-	/*		Seems like it will be deleted
+	// Create a message with the content
+	event.reply(
+		content.set_flags(dpp::m_ephemeral)
+	);
 	
 	client.on_select_click([&client](const dpp::select_click_t& event) {
-		event.reply("You clicked " + event.custom_id + " and chose: " + event.values[0]);
+		event.reply(
+			dpp::message()
+			.set_flags(dpp::m_ephemeral)
+			.set_content("You clicked " + event.custom_id + " and chose: " + event.values[0])
+		);
+
+		// Interaction reply check
+		fmt::print(
+			"[running] info command - selection replied from {}\n",
+			event.command.usr.format_username()
+		);
 		});
 
-	*/
 
 	/* ---------------------------------------------------------------------- */
 
