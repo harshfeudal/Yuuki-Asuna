e638d3c488374043ab09836e3962afd40dffb55d
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-06-18 20:24:55 +0700

    Asuna can kick now ayyy

--- Yuuki Asuna/src/kick.cpp
+++ Yuuki Asuna/src/kick.cpp
@@ -9,9 +9,15 @@ void kick_h(dpp::cluster& client, const dpp::slashcommand_t& event)
 {
 	// Target a user mentioned
 	auto target_user = event.get_parameter("member");
+	dpp::snowflake user_targeted = std::get<dpp::snowflake>(target_user);
 
 	// Making reason
-	auto reason = event.get_parameter("reason");
+	auto target_reason = event.get_parameter("reason");
+	std::string reason = std::get<std::string>(target_reason);
+
+	// Guild find and get the guild ID
+	auto guild_find = dpp::find_guild(event.command.guild_id);
+	dpp::snowflake guild_target = event.command.guild_id;
 	
 	/*
 		Note:
@@ -24,39 +30,22 @@ void kick_h(dpp::cluster& client, const dpp::slashcommand_t& event)
 
 	/* ------------------- I still making it, please wait ------------------- */
 
-	// .has() permission is planning ...
-
-	auto* guild_find = dpp::find_guild(event.command.guild_id);
 	if (guild_find->base_permissions(&event.command.usr).has(dpp::p_kick_members))
 	{
-		// client.guild_member_kick(guild_find, target_user);
-		// client.set_audit_reason(reason);
+		client.set_audit_reason(reason);
+		client.guild_member_kick(guild_target, user_targeted);
+
+		// Remake the reply section
+
+		// Reply when got kicked
+		event.reply(
+			dpp::message()
+			.set_flags(dpp::m_ephemeral)
+			.set_content("kicked!")
+		);
 	}
 
-	/*
-	Advise code:
-
-	Type 1:
-	std::accumulate(guild_member.roles.cbegin(), guild_member.roles.cend(), false, [](bool ret, dpp::role const& role) -> bool {
-		return ret || role.has_ban_members();
-		}));
-	
-	Type 2:
-	auto* guild = dpp::find_guild(event.command.guild_id);
-	if (!guild) { handle uncached guild case }
-	if (guild->base_permissions(&event.command.usr).has(dpp::p_ban_members)) {
-		
-	}
-	*/
-
-	/* ---------------------------------------------------------------------- */
-
-	// Under-testing if this works (will deleted this command soon)
-	event.reply(
-		dpp::message()
-		.set_flags(dpp::m_ephemeral)
-		.set_content("Under-construction kick command")
-	);
+	/* ---------------------------------------------------------------------- */	
 
 	// Interaction reply check
 	fmt::print(
