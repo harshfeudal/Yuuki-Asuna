325ae49941059b2a1852e591168f063516935864
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-06-18 23:52:57 +0700

    update some new things

--- Yuuki Asuna/src/kick.cpp
+++ Yuuki Asuna/src/kick.cpp
@@ -13,35 +13,52 @@ void kick_h(dpp::cluster& client, const dpp::slashcommand_t& event)
 
 	// Making reason
 	auto target_reason = event.get_parameter("reason");
-	std::string reason = std::get<std::string>(target_reason);
+
+	// Check if we have reason or not
+	if (std::holds_alternative<std::string>(target_reason)) 
+	{
+		std::string reason = std::get<std::string>(target_reason);
+		client.set_audit_reason(reason);
+	}
+	else
+	{
+		std::string reason = "No reason provided";
+		client.set_audit_reason(reason);
+	}
 
 	// Guild find and get the guild ID
 	auto guild_find = dpp::find_guild(event.command.guild_id);
 	dpp::snowflake guild_target = event.command.guild_id;
+
+	// Find the command.usr role
+	// auto command_target_user_role = dpp::find_role();
 	
 	/*
 		Note:
-		- Permission will be checked using Dpp system,
-		I no need to make a single check like Discord.js
-		- It's still under-construction!
+			- It's still under-construction!
+			- Role check is still under process!
 		
 		Thanks!
 	*/
 
 	/* ------------------- I still making it, please wait ------------------- */
 
+	// Role check is making ...
+
+	// Guild finding and permission checking
 	if (guild_find->base_permissions(&event.command.usr).has(dpp::p_kick_members))
 	{
-		client.set_audit_reason(reason);
 		client.guild_member_kick(guild_target, user_targeted);
 
 		// Remake the reply section
 
 		// Reply when got kicked
+		std::string kick_ephemeral_content = fmt::format("<@{}> has been kicked!", user_targeted);
+
 		event.reply(
 			dpp::message()
 			.set_flags(dpp::m_ephemeral)
-			.set_content("kicked!")
+			.set_content(kick_ephemeral_content)
 		);
 	}
 
