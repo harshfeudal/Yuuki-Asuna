c86ad9ba4c8773698635019c78f02feb7edfbb12
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-06-19 12:16:02 +0700

    update something, but it has sth null

--- Yuuki Asuna/src/kick.cpp
+++ Yuuki Asuna/src/kick.cpp
@@ -27,7 +27,7 @@ void kick_h(dpp::cluster& client, const dpp::slashcommand_t& event)
 	}
 
 	// Guild find and get the guild ID
-	auto guild_find = dpp::find_guild(event.command.guild_id);
+	auto* guild_find = dpp::find_guild(event.command.guild_id);
 	dpp::snowflake guild_target = event.command.guild_id;
 
 	// Find the command.usr role
@@ -37,6 +37,8 @@ void kick_h(dpp::cluster& client, const dpp::slashcommand_t& event)
 		Note:
 			- It's still under-construction!
 			- Role check is still under process!
+			- I decide to make button too, since we need to confirm that they want to kick,
+			not a mistake.
 		
 		Thanks!
 	*/
@@ -45,22 +47,40 @@ void kick_h(dpp::cluster& client, const dpp::slashcommand_t& event)
 
 	// Role check is making ...
 
-	// Guild finding and permission checking
-	if (guild_find->base_permissions(&event.command.usr).has(dpp::p_kick_members))
+	// Button is making
+
+	// If the interaction location is not a guild
+	if (!guild_find)
 	{
-		client.guild_member_kick(guild_target, user_targeted);
-
-		// Remake the reply section
-
-		// Reply when got kicked
-		std::string kick_ephemeral_content = fmt::format("<@{}> has been kicked!", user_targeted);
-
 		event.reply(
 			dpp::message()
 			.set_flags(dpp::m_ephemeral)
-			.set_content(kick_ephemeral_content)
+			.set_content("You cannot use this command outside the server")
 		);
 	}
+
+	// Check permission: if they don't have KICK_MEMBERS permission
+	if (!(guild_find->base_permissions(&event.command.usr).has(dpp::p_kick_members)))
+	{
+		event.reply(
+			dpp::message()
+			.set_flags(dpp::m_ephemeral)
+			.set_content("You have lack of permission to kick :(")
+		);
+	}
+
+	client.guild_member_kick(guild_target, user_targeted);
+
+	// Remake the reply section
+
+	// Reply when got kicked
+	std::string kick_ephemeral_content = fmt::format("<@{}> has been kicked!", user_targeted);
+
+	event.reply(
+		dpp::message()
+		.set_flags(dpp::m_ephemeral)
+		.set_content(kick_ephemeral_content)
+	);
 
 	/* ---------------------------------------------------------------------- */	
 
